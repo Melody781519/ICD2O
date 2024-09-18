feaaa0af2a0d5a11a75e851f045686b42c68de18
Melody781519 <781519@pdsb.net>
2024-09-18 14:05:46 -0400

    Update myo_random.java

--- myo_random.java
+++ myo_random.java
@@ -1,2 +1,11 @@
 class myo_random [
 ]
+class Testing {
+  public static void main(String [] args) {
+    // place your code here!
+// this is a code comment
+String name = "Melody";
+System.out.println("Hello " + name);
+    // your code ends here!
+  }
+}
